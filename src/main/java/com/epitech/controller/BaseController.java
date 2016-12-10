9600c60c5e8e2cca27e3399e4e25f7cbb55b96e1
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-10 16:12:30 +0100

    adding leaf support for spring + Templates layouts

--- src/main/java/com/epitech/controller/BaseController.java
+++ src/main/java/com/epitech/controller/BaseController.java
@@ -1,16 +1,19 @@
 package com.epitech.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sakiir on 09/12/16.
  */
-@RestController
+@Controller
 public class BaseController {
-
-    @RequestMapping("/")
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.GET
+    )
     public String   index() {
-        return "you are on /";
+        return "index";
     }
 }
