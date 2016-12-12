2c891d99bada25aea7b646c14180f72d3afea37f
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-12 19:23:02 +0100

    adding login action

--- src/main/java/com/epitech/controller/UserController.java
+++ src/main/java/com/epitech/controller/UserController.java
@@ -1,17 +1,31 @@
 package com.epitech.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sakiir on 10/12/16.
  */
 @Controller
 public class            UserController {
-    @RequestMapping("/login")
+    @RequestMapping(
+            value  = "/login",
+            method = RequestMethod.GET
+    )
     public String       login() {
         return "login";
     }
+
+    @RequestMapping(
+            value  = "/login",
+            method = RequestMethod.POST
+    )
+    public String       login(@RequestBody String body) {
+        // parse body and find user in database :)
+        return "login";
+    };
 
     @RequestMapping("/register")
     public String       register() {
