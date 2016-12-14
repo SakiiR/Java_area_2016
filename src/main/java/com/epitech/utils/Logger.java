20eebac00fddb70e7255bdf9a6351ff7cc58215d
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-14 17:04:29 +0100

    Fixing logger + test adding

--- src/main/java/com/epitech/utils/Logger.java
+++ src/main/java/com/epitech/utils/Logger.java
@@ -1,7 +1,5 @@
 package com.epitech.utils;
 
-import java.util.Objects;
-
 /**
  * Created by sakiir on 14/12/16.
  */
@@ -17,16 +15,74 @@ public class                    Logger {
      * @param fmt the format string
      * @param params the var args params
      */
-    public static final void    logInfo(String fmt, Objects ...params) {
-        System.out.println(String.format("[\033[34m!\033[0m] " + fmt, params));
+    public static final void    logInfo(String fmt, Object ...params) {
+        System.out.println(String.format("[\033[34m^\033[0m] " + "\033[34m" + fmt + "\033[0m", params));
     }
 
     /**
      * Log a blue message to the console.
      *
-     * @param str
+     * @param str the string to display
      */
     public static final void    logInfo(String str) {
-        System.out.println("[\033[34m!\033[0m] " + str);
+        System.out.println("[\033[34m^\033[0m] " + "\033[34m" + str + "\033[0m");
     }
+
+    /**
+     * Log a green message to the console
+     *
+     * @param fmt the format string
+     * @param params the var args params
+     */
+    public static final void    logSuccess(String fmt, Object ...params) {
+        System.out.println(String.format("[\033[32m+\033[0m] " + "\033[32m" + fmt + "\033[0m", params));
+    }
+
+    /**
+     * Log a green message to the console
+     *
+     * @param str the string to display
+     */
+    public static final void    logSuccess(String str) {
+        System.out.println("[\033[32m+\033[0m] " + "\033[32m" + str + "\033[0m");
+    }
+
+    /**
+     * Log an orange message to the console.
+     *
+     * @param fmt the format string
+     * @param params the var args params
+     */
+    public static final void    logWarning(String fmt, Object ...params) {
+        System.out.println(String.format("[\033[33m!\033[0m] " + "\033[33m" + fmt + "\033[0m", params));
+    }
+
+    /**
+     * Log an orange message to the console.
+     *
+     * @param str the string to display
+     */
+    public static final void    logWarning(String str) {
+        System.out.println("[\033[33m!\033[0m] " + "\033[33m" + str + "\033[0m");
+    }
+
+    /**
+     * Log a red message to the console.
+     *
+     * @param fmt the format string
+     * @param params the var args params
+     */
+    public static final void    logError(String fmt, Object ...params) {
+        System.out.println(String.format("[\033[31m-\033[0m] " + "\033[31m" + fmt + "\033[0m", params));
+    }
+
+    /**
+     * Log a red message to the console.
+     *
+     * @param str the string to display
+     */
+    public static final void    logError(String str) {
+        System.out.println("[\033[31m-\033[0m] " + "\033[31m" + str + "\033[0m");
+    }
+
 }
---
+++ src/test/java/com/epitech/LoggerTest.java
@@ -0,0 +1,46 @@
+package com.epitech;
+
+import com.epitech.utils.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by sakiir on 14/12/16.
+ */
+
+@RunWith(SpringRunner.class)
+public class            LoggerTest {
+
+    @Test
+    public void         infoLogTest() {
+        System.out.println("[TEST] Testing logInfo without format ..");
+        Logger.logInfo("INFO !");
+        System.out.println("[TEST] Testing logInfo with format ..");
+        Logger.logInfo("INFO ! %s", "foo");
+    }
+
+    @Test
+    public void         successLogTest() {
+        System.out.println("[TEST] Testing logSuccess without format ..");
+        Logger.logSuccess("SUCCESS !");
+        System.out.println("[TEST] Testing logSuccess with format ..");
+        Logger.logSuccess("SUCCESS ! %s", "foo");
+    }
+
+    @Test
+    public void         warningLogTest() {
+        System.out.println("[TEST] Testing logWarning without format ..");
+        Logger.logWarning("WARNING !");
+        System.out.println("[TEST] Testing logWarning with format ..");
+        Logger.logWarning("WARNING ! %s", "foo");
+    }
+
+    @Test
+    public void         errorLogTest() {
+        System.out.println("[TEST] Testing logError without format ..");
+        Logger.logError("ERROR !");
+        System.out.println("[TEST] Testing logError with format ..");
+        Logger.logError("ERROR ! %s", "foo");
+    }
+}
