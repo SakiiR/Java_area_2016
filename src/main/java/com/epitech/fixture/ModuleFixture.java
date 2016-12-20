2a57e8be45bbf219fa72abe69a7b8a97b2a41f70
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-20 15:48:36 +0100

    Added odule fixture

---
+++ src/main/java/com/epitech/fixture/ModuleFixture.java
@@ -0,0 +1,9 @@
+package com.epitech.fixture;
+
+/**
+ * Created by sakiir on 20/12/16.
+ */
+public class                ModuleFixture {
+    public static void      init() {  }
+    public static void      clear() {  }
+}
