e0df74b6241bef929db3cc56f7d69536286d6330
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-22 10:55:24 +0100

    Added oauth action

--- src/main/java/com/epitech/controller/ModuleController.java
+++ src/main/java/com/epitech/controller/ModuleController.java
@@ -8,14 +8,17 @@ import com.epitech.service.FacebookService;
 import com.epitech.service.IService;
 import com.epitech.utils.BodyParser;
 import com.epitech.utils.Logger;
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import com.epitech.model.Module;
 import com.epitech.repository.ModuleRepository;
+import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpSession;
+import java.util.Base64;
 import java.util.List;
 import java.lang.reflect.Constructor;
 
@@ -87,5 +90,38 @@ public class                        ModuleController {
         } else modelMap.addAttribute("message", "Missing fields");
         modelMap.addAttribute("redirectUrl", "/module/list");
         return "module/manage.html";
+    }
+
+    @RequestMapping(value = "/module/oauth", method = RequestMethod.GET, params = {"access_token", "state"})
+    public String                   oauth(HttpSession httpSession, ModelMap modelMap, @RequestParam(value = "access_token") String access_token, @RequestParam(value = "state") String state) {
+        BodyParser                  bodyParser = new BodyParser(new String(Base64.getDecoder().decode(state)));
+        String                      stateType = bodyParser.get("type");
+        String                      stateUsername = bodyParser.get("username");
+        String                      connectedUser = (String) httpSession.getAttribute("username");
+
+        if (null == connectedUser) {
+            return "redirect:../login";
+        }
+
+        if (!(stateType == null || stateUsername == null)) {
+            User user = userRepository.findByUsername(connectedUser);
+            if (!(user == null)) {
+                UserModule userModule = new UserModule();
+                Module module = moduleRepository.findByName(stateType);
+
+                if (!(module == null)) {
+                    userModule.setModule(module)
+                            .setUser(user)
+                            .setToken(access_token);
+                    userModuleRepository.save(userModule);
+                    user.addModule(userModule);
+                    userRepository.save(user);
+                    modelMap.addAttribute("success", true);
+                    modelMap.addAttribute("message", String.format("You have been connected to %s", stateType));
+                } else modelMap.addAttribute("message", "State's Type is unknown !");
+            } else modelMap.addAttribute("message", "Who are you ?!!");
+        } else modelMap.addAttribute("message", "Missing type or username in callback state");
+
+        return "module/oauth.html";
     }
 }
