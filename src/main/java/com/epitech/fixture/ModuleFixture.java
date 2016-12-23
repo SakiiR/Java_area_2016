4ed2c440c32e96cdf200dc42fc3ece5e30480bd2
Karine Aknin <karine.aknin@epitech.eu>
2016-12-23 20:40:12 +0100

    add new url fixtures

--- src/main/java/com/epitech/fixture/ModuleFixture.java
+++ src/main/java/com/epitech/fixture/ModuleFixture.java
@@ -29,12 +29,12 @@ public class                ModuleFixture {
         this.moduleRepository.save(new Module("twitter", "/img/modules/twitter.png", "Description for twitter", ""));
         this.moduleRepository.save(new Module("epitech","/img/modules/intra-epitech.png","Description for intra epitech", ""));
         this.moduleRepository.save(new Module("office","/img/modules/office.png","Description for office 365", ""));
-        this.moduleRepository.save(new Module("gmail","/img/modules/gmail.png","Description for gmail/inbox", ""));
-        this.moduleRepository.save(new Module("yammer","/img/modules/yammer.png","Description for yammer", ""));
+        this.moduleRepository.save(new Module("gmail","/img/modules/gmail.png","Description for gmail/inbox", "https://accounts.google.com/o/oauth2/v2/auth?client_id=801969793224-jh02oeqpfvi2ocjrebbp21dks178qav5.apps.googleusercontent.com&redirect_uri=http://localhost:8080/module/oauth&response_type=token&scope=email"));
+        this.moduleRepository.save(new Module("yammer","/img/modules/yammer.png","Description for yammer", "https://www.yammer.com/oauth2/authorize?client_id=zPX7lhZerhGVn9beAAfGg&redirect_uri=http://localhost:8080/module/oauth&response_type=token"));
         this.moduleRepository.save(new Module("onedrive","/img/modules/onedrive.png","Description for onedrive", ""));
-        this.moduleRepository.save(new Module("googledrive", "/img/modules/googledrive.png", "Description for googledrive", ""));
+        this.moduleRepository.save(new Module("googledrive", "/img/modules/googledrive.png", "Description for googledrive", "https://accounts.google.com/o/oauth2/v2/auth?client_id=801969793224-jh02oeqpfvi2ocjrebbp21dks178qav5.apps.googleusercontent.com&redirect_uri=http://localhost:8080/module/oauth&response_type=token&scope=https://www.googleapis.com/auth/drive"));
         this.moduleRepository.save(new Module("slack", "/img/modules/slack.png", "Description for slack", ""));
-        this.moduleRepository.save(new Module("github", "/img/modules/github.png", "Description for github", ""));
+        this.moduleRepository.save(new Module("github", "/img/modules/github.png", "Description for github", "https://github.com/login/oauth/authorize?client_id=d6b6716e70d77f94200e&redirect_uri=http://localhost:8080/module/oauth&response_type=token&scope=repo"));
         this.moduleRepository.save(new Module("youtube", "/img/modules/youtube.png", "Description for youtube", ""));
         this.moduleRepository.save(new Module("pastebin", "/img/modules/pastebin.png", "Description for pastebin", ""));
     }
