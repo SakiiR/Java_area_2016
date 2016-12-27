51d8b39e2dbe283b6d5f7a83b93623e5743cd1cb
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-27 18:16:33 +0100

    added PastebinTest + login()

--- src/main/java/com/epitech/service/PastebinService.java
+++ src/main/java/com/epitech/service/PastebinService.java
@@ -1,9 +1,39 @@
 package com.epitech.service;
 
-public class            PastebinService implements IService {
-    public              PastebinService() {}
+import com.epitech.utils.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-    public String       login(String username, String password) {
-        return "PASTEBIN_TOKEN";
+public class                                PastebinService implements IService {
+    private RestTemplate                    rest;
+
+    public                                  PastebinService() {
+        this.rest = new RestTemplate();
+    }
+
+    public static String                    api_dev_key = "221ed12ce0e20b3d32fed3b3545346f7";
+
+    public String                           login(String username, String password) {
+        MultiValueMap<String, String>       postParameters = new LinkedMultiValueMap<>();
+        String                              result = null;
+        HttpHeaders                         headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        postParameters.add("api_dev_key", PastebinService.api_dev_key);
+        postParameters.add("api_user_name", username);
+        postParameters.add("api_user_password", password);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(postParameters, headers);
+        ResponseEntity<String> response = this.rest.postForEntity("http://pastebin.com/api/api_login.php", request, String.class);
+        if (!(response.getBody().contains("Bad API request"))) {
+            result = response.getBody();
+        }
+        return result;
     }
 }
---
+++ src/test/java/com/epitech/PastebinServiceTest.java
@@ -0,0 +1,28 @@
+package com.epitech;
+
+import com.epitech.service.PastebinService;
+import com.epitech.utils.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.rmi.runtime.Log;
+
+/**
+ * Created by sakiir on 27/12/16.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class            PastebinServiceTest {
+    @Test
+    public void         loginTest() {
+        String username = "sakiir";
+        String password = "123";
+        String result;
+
+        PastebinService pastebinService = new PastebinService();
+        result = pastebinService.login(username, password);
+        Logger.logInfo("[TEST] Pastebin Result : %s", result);
+        assert (result == null);
+    }
+}
