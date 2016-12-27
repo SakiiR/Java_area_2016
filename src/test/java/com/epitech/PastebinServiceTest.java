7c6f0721af38ea01b664e7d12cdc057c7aac29cc
Erwan Dupard <erwan.dupard@epitech.eu>
2016-12-27 18:18:04 +0100

    Fixed indentation

--- src/test/java/com/epitech/PastebinServiceTest.java
+++ src/test/java/com/epitech/PastebinServiceTest.java
@@ -6,11 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.rmi.runtime.Log;
 
-/**
- * Created by sakiir on 27/12/16.
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class            PastebinServiceTest {
