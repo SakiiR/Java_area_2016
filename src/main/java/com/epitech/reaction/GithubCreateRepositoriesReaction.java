8fdacb21b7183795b1a19730ab8b53fb5dfcb7c8
Stephane Karraz <stephane.karraz@epitech.eu>
2017-01-04 16:11:50 +0100

    Seems ok

---
+++ src/main/java/com/epitech/reaction/GithubCreateRepositoriesReaction.java
@@ -0,0 +1,30 @@
+package com.epitech.reaction;
+
+import com.epitech.utils.ErrorCode;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.kohsuke.github.GitHub;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Segmev on 04/01/17.
+ */
+public class GithubCreateRepositoriesReaction implements IReaction {
+    private String          token;
+
+    public                  GithubCreateRepositoriesReaction(String token) { this.token = token ; }
+
+    @Override
+    public ErrorCode        run(Object data) {
+        try {
+            ArrayList<String> repositories = (ArrayList<String>) data;
+            GitHub hub = GitHub.connectUsingOAuth(this.token);
+            for (String name: repositories) {
+                hub.createRepository(name).create();
+            }
+        } catch (Exception e) {
+            return ErrorCode.UNKNOWN;
+        }
+        return ErrorCode.SUCCESS;
+    }
+}
