a6ccef8f76741e76a62d8fd6255d3f0ad82e9d9c
Karine Aknin <karine.aknin@epitech.eu>
2017-01-04 18:25:58 +0100

    merge

--- src/main/java/com/epitech/fixture/ModuleFixture.java
+++ src/main/java/com/epitech/fixture/ModuleFixture.java
@@ -36,7 +36,7 @@ public class                    ModuleFixture {
         this.add(new Module("dropbox","/img/modules/dropbox.png","Description for dropbox", "https://www.dropbox.com/1/oauth2/authorize?response_type=token&client_id=whfnl4mepngbvsb&redirect_uri=http://localhost:8080/module/oauth"));
         this.add(new Module("drive", "/img/modules/drive.png", "Description for drive", "https://accounts.google.com/o/oauth2/v2/auth?client_id=801969793224-jh02oeqpfvi2ocjrebbp21dks178qav5.apps.googleusercontent.com&redirect_uri=http://localhost:8080/module/oauth&response_type=token&scope=https://www.googleapis.com/auth/drive"));
         this.add(new Module("slack", "/img/modules/slack.png", "Description for slack", ""));
-        this.add(new Module("github", "/img/modules/github.png", "Description for github", "https://github.com/login/oauth/authorize?client_id=d6b6716e70d77f94200e&redirect_uri=http://localhost:8080/module/oauth&response_type=token&scope=repo"));
+        this.add(new Module("github", "/img/modules/github.png", "Description for github", "https://github.com/login/oauth/authorize?client_id=d6b6716e70d77f94200e&response_type=token&redirect_uri=http://localhost:8080/module/oauth&scope=repo"));
         this.add(new Module("youtube", "/img/modules/youtube.png", "Description for youtube", ""));
         this.add(new Module("pastebin", "/img/modules/pastebin.png", "Description for pastebin", ""));
     }
